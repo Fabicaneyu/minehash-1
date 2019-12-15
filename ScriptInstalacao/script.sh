ab6ea5fe9e4087b48faaf6ef1aa59e4207581ee8
Nathymedina2305 <natalia.juliano@hotmail.com>
2019-12-15 11:30:09 -0300

    Script final

--- ScriptInstalacao/script.sh
+++ ScriptInstalacao/script.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 function central(){
            copia_bin
-           validacao_java
-           jar
 }
 
 function copia_bin(){
            bin_jar=$(ls /usr/local/bin/  |  grep   minehashh.jar)
-           grep_script=$(ls /usr/local/bin  |  grep script_instalacao)
-           if [ $bin_jar  -z  ]  2> /dev/null
+           grep_=$(ls /usr/local/bin  |  grep _instalacao)
+           if [ $bin_jar  -z  ]  2>/dev/null
            then
-           sudo cp minehashh.jar    /usr/local/bin  2> /dev/null
-           elif    [  $bin_script  -z  ]   2> /dev/null
+           sudo cp minehashh.jar    /usr/local/bin  2>/dev/null
+	   validacao_java
+           elif    [  $bin_  -z  ]   2>/dev/null
            then
-sudo java -jar /usr/local/bin/minehashh.jar
+	   jar
 
 fi
 }
 
 function validacao_java(){
 
-which java | grep –q  java  2> /dev/null
+which java | grep -q  java  2>/dev/null
 
 if [  $? -eq  0  ]   
-	then echo “Instalando minehash”
+	then echo "Instalando minehash"
 else
 	sudo apt-get install openjdk-8-jdk
 fi
@@ -34,7 +33,7 @@ function jar(){
 java -jar minehashh.jar    2>/dev/null
                if [  $?  -ne  0  ]
 	then 
-	sudo java -jar / usr/local/bin/minehashh.jar
+	sudo java -jar /usr/local/bin/minehashh.jar
  fi
 } 
 central 
